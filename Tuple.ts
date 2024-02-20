2d74d0065beed28e8d7c77885d00deb22a1d336a
Fairy-Heart <mavis.ngoc.nguyen@gmail.com>
2024-02-20 17:56:04 +0700

    TypeScript Programming for beginner

---
+++ Tuple.ts
@@ -0,0 +1,11 @@
+let myTuple : [string, number] = ['Mavis', 1];
+/*
+IMPORTANT: Unlike arrays, tuple data types 
+need to be entered in the correct order 
+that has been set., for example, if you set :
+let MyTupleError : [string, number] = [1, 'Mavis'];
+output : Error
+
+*/
+
+console.log(`${myTuple[0]}`);
