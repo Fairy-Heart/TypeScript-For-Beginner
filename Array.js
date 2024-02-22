9b6bbf720716c79a20cddecbb5b406b8e997afe0
Fairy-Heart <142316777+Fairy-Heart@users.noreply.github.com>
2024-02-22 20:51:17 +0700

    Delete Array.js

--- Array.js
+++
@@ -1,4 +0,0 @@
-var myArray = ['My String'];
-//Array many type :
-var myArray2 = ['Hello world', true];
-console.log("".concat(myArray[0], ", ").concat(myArray2[0], ", ").concat(myArray2[1])); //Ouput : My String, Hello World, true
