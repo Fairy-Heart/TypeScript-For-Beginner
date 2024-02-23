39139f580a843a3f56ee719897b14b2787c2482a
Fairy-Heart <mavis.ngoc.nguyen@gmail.com>
2024-02-24 02:38:04 +0700

    added new file

---
+++ JavaScript/switch.js
@@ -0,0 +1,14 @@
+var int_number = 3;
+switch (int_number) {
+    case 1:
+        console.log("".concat(int_number, " = 1"));
+        break;
+    case 2:
+        console.log("".concat(int_number, " == 2"));
+        break;
+    case 3:
+        console.log("".concat(int_number, " == 3"));
+        break;
+    default:
+        console.log("".concat(int_number, " is not a number or > 3"));
+}
---
+++ TypeScript/switch.ts
@@ -0,0 +1,15 @@
+let int_number : number = 3;
+
+switch(int_number) {
+	case 1:
+	console.log(`${int_number} = 1`);
+	break;
+	case 2:
+	console.log(`${int_number} == 2`);
+	break;
+	case 3:
+	console.log(`${int_number} == 3`);
+	break;
+	default:
+	console.log(`${int_number} is not a number or > 3`);
+}
