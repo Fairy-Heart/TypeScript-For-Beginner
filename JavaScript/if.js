fabdfea69ba3c540e4958773292c2f14400ef581
Fairy-Heart <mavis.ngoc.nguyen@gmail.com>
2024-02-24 01:57:47 +0700

    added TypeScript and JavaScript codes

---
+++ JavaScript/if.js
@@ -0,0 +1,10 @@
+var num = 12;
+if (num < 12) {
+    console.log("".concat(num, " < 12"));
+}
+else if (num == 12) {
+    console.log("".concat(num, " == 12"));
+}
+else {
+    console.log("".concat(num, " > 12"));
+}
---
+++ TypeScript/if.ts
@@ -0,0 +1,9 @@
+let num : number = 12;
+
+if(num < 12) {
+	console.log(`${num} < 12`);
+} else if (num == 12) {
+	console.log(`${num} == 12`);
+} else {
+	console.log(`${num} > 12`);
+}
