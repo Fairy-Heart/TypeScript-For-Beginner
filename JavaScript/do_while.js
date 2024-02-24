5f61e15df9c262ea3eba0be4344402ba0030ee00
Fairy-Heart <mavis.ngoc.nguyen@gmail.com>
2024-02-25 02:13:59 +0700

    added new file

---
+++ JavaScript/do_while.js
@@ -0,0 +1,5 @@
+var num_counter = 0;
+do {
+    console.log("".concat(num_counter));
+    num_counter++;
+} while (num_counter < 10);
---
+++ TypeScript/do_while.ts
@@ -0,0 +1,6 @@
+let num_counter : number = 0;
+
+do {
+	console.log(`${num_counter}`);
+	num_counter++
+} while(num_counter < 10);
